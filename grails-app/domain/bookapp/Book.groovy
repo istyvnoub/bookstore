99567ffcbaea810c2017737fe26509efb8d90b0b
Martin Stein <m.stein@gsi.de>
2020-09-04 12:13:59 +0200

    Renamed old Books domain class to Book because of the convention to use singular names for classes rather than plural names

---
+++ grails-app/domain/bookapp/Book.groovy
@@ -0,0 +1,15 @@
+package bookapp
+
+class Book {
+    String name
+    String isbn
+    Float price
+
+    static belongTo =[author: Author]
+
+    static constraints = {
+        name blank: false, nullable: false
+        isbn blank: false, nullable: false
+        price nullable: false
+    }
+}
