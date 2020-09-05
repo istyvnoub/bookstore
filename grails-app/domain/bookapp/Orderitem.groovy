68f302cd97bc6aa006fc2a46564820e68e0346c6
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-09-05 15:02:59 +0200

    Order item added

--- grails-app/domain/bookapp/Orderitem.groovy
+++ grails-app/domain/bookapp/Orderitem.groovy
@@ -3,7 +3,7 @@ package bookapp
 class Orderitem {
     Integer qty
     Float total
-    static belongsTo=[orders:OnlineOrder, product:Books]
+    static belongsTo=[orders:OnlineOrder, product:Book]
 
 
     static constraints = {
