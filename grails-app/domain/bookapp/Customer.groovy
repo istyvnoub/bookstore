117d076e82353f27fe522c51c85b7090c71ce26b
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-09-05 15:02:30 +0200

    Customer added

--- grails-app/domain/bookapp/Customer.groovy
+++ grails-app/domain/bookapp/Customer.groovy
@@ -3,10 +3,10 @@ package bookapp
 class Customer {
      String firstName
     String lastName
-    Long phone
+
     String email
-    Integer totalPoint
-    static hasMany=[books:Books]
+
+    static hasMany=[books:Book]
 
     static constraints = {
 
@@ -14,7 +14,7 @@ class Customer {
         firstName blank: false
         lastName  blank: false
         email nullable: true
-        totalPoint nullable:true
+
     }
     static mapping={
         version(false)
