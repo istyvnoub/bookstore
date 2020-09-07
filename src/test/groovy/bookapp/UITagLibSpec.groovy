1ab66259cc9e0d24527b4725196f93f8a633afcb
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-09-07 22:30:45 +0200

    Tag added

---
+++ src/test/groovy/bookapp/UITagLibSpec.groovy
@@ -0,0 +1,18 @@
+package bookapp
+
+import grails.testing.web.taglib.TagLibUnitTest
+import spock.lang.Specification
+
+class UITagLibSpec extends Specification implements TagLibUnitTest<UITagLib> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
