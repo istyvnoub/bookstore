d74176a197b925baf4b57cb7a6e3701bfe490d60
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-09-07 22:32:46 +0200

    tags added

---
+++ grails-app/taglib/bookapp/UITagLib.groovy
@@ -0,0 +1,16 @@
+package bookapp
+
+class UITagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    static namespace = "admin"
+    def renderErrormessage= { attrs ,body ->                          //Groovy closure
+    def model=attrs.model
+        String fieldName=attrs.fielName
+        String errorMessage = attrs.errorMessage? g.message(code: attrs.errorMessage): g.message(code: "invalid.input")
+        if (model && model.errors && model.errors.getFieldError(fieldName)){
+            out << "<small class='form-text text-danger''><strong>${errorMessage}</strong></small>"
+        }
+    }
+}
