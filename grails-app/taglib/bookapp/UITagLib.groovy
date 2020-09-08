6526db1827974eeeb304468021638e67a2555f6a
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-09-08 13:05:16 +0200

    memberActionMenu added

--- grails-app/taglib/bookapp/UITagLib.groovy
+++ grails-app/taglib/bookapp/UITagLib.groovy
@@ -1,9 +1,9 @@
 package bookapp
 
 class UITagLib {
-    static defaultEncodeAs = [taglib:'html']
+  //  static defaultEncodeAs = [taglib:'html']
     //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
-
+    AuthenticationService authenticationService
     static namespace = "admin"
     def renderErrormessage= { attrs ,body ->                          //Groovy closure
     def model=attrs.model
@@ -12,5 +12,12 @@ class UITagLib {
         if (model && model.errors && model.errors.getFieldError(fieldName)){
             out << "<small class='form-text text-danger''><strong>${errorMessage}</strong></small>"
         }
+    }
+    def memberActionMenu = { attrs, body ->
+        out << '<li class="nav-item dropdown show">'
+        out << g.link(class:"nav-link dropdown-toggle", "data-toggle":"dropdown"){authenticationService.getMemberName()}
+        out << '<div class="dropdown-menu">'
+        out << g.link(controller: "authentication", action: "logout", class: "dropdown-item"){g.message(code:"logout")}
+        out << "</div></li>"
     }
 }
